3152d998caf39d144c9130bb5010ed852ab21a61
mikeymckay <mikeymckay@gmail.com>
2013-07-10 16:00:25 -0400

    Small fixes to alert script

--- scripts/dailyAlertCheck.rb
+++ scripts/dailyAlertCheck.rb
@@ -50,12 +50,19 @@ Capybara::Screenshot.autosave_on_failure = false
 Capybara.save_and_open_page_path = "/tmp"
 
 def hide_everything_except(id)
+  page.find_by_id(id)  #Makes sure capybara waits
   page.execute_script("$('##{id}').siblings().hide();$('##{id}').parents().siblings().hide();$('div[data-role=page]').css('min-height','')")
 end
 
 def remove_everything_except(id)
+  page.find_by_id(id)  #Makes sure capybara waits
   hide_everything_except(id)
   page.execute_script("$(':hidden').remove()")
+end
+
+def get_element_html(id)
+  page.find_by_id(id)  #Makes sure capybara waits
+  page.evaluate_script("$('##{id}').html()")
 end
 
 include Capybara::DSL
@@ -70,9 +77,11 @@ end
 
 def daily_alert_check
   visit('#reports/reportType/alerts')
-  page.find_by_id("hasAlerts")
-  remove_everything_except("alerts")
-  return page.find_by_id("alerts").html
+  if get_element_html("hasAlerts").match(/Report finished, alerts found./)
+    return get_element_html("alerts")
+  else
+    return nil
+  end
 end
 
 
@@ -90,7 +99,11 @@ end
 login()
 puts "Logged in"
 puts "Getting alerts"
-puts "Sending email to: #{opts.send_to}"
-send_email(opts.send_to.split(","),daily_alert_check())
+alerts = daily_alert_check()
+if alerts.nil?
+  puts "No alerts found"
+else
+  puts "Alerts found, sending email to: #{opts.send_to}"
+  send_email(opts.send_to.split(","),alerts) unless alerts.nil?
+end
 puts "Done"
-
