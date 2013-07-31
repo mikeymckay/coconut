e2bba89984baac3bcbccc975a0a1d63d4f5228e5
fet mar <fetmar@gmail.com>
2013-07-31 12:53:35 -0400

    directory restructuring

--- coconut.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Content-Type');
-var_dump($_POST)
-?>
