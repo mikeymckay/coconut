74b130d80cde0bb77950ff42e5fa835645f2edea
fet mar <fetmar@gmail.com>
2013-08-07 18:27:38 -0400

    stop tracking android compiled files

--- cordova/gen/R.java.d
+++
@@ -1,7 +0,0 @@
-/var/www/coconut/build_android_app/gen/com/couchbase/callback/R.java \
- : /var/www/coconut/build_android_app/res/values/strings.xml \
-/var/www/coconut/build_android_app/res/xml/phonegap.xml \
-/var/www/coconut/build_android_app/res/xml/plugins.xml \
-/var/www/coconut/build_android_app/res/drawable/icon.png \
-/var/www/coconut/build_android_app/res/layout/main.xml \
-/var/www/coconut/build_android_app/bin/AndroidManifest.xml \
